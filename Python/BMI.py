9d74ec76a2a8f567aa16e856b755aa73efb1b245
Rens Junior Sibarani <58243292+ifs19052@users.noreply.github.com>
2022-10-10 16:05:57 +0700

    Update

--- Python/BMI.py
+++ Python/BMI.py
@@ -1,7 +1,7 @@
 height = float(input("Enter height in meters "))
 weight = float(input("Enter weight in kg "))
 bmi = weight/(height**2)
-print("Your BMI is : {0} and you are: ".format((bmi),end='')
+print("Your BMI is : {0} and you are: ".format((bmi),end=''))
 if (bmi < 16):
 	print("Severely underweight")
 elif (bmi >= 16 and bmi <18.5):
--- Toki/Pemrograman Dasar/ATambahB.c
+++ Toki/Pemrograman Dasar/ATambahB.c
@@ -6,6 +6,6 @@ int main()
 
 	scanf("%d %d", &a, &b);
 
-	printf("%d\n", a+b);
+	printf("%d\n", a + b);
 	return 0;
 }
